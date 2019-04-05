bd8354f42606f8bb4c20a2422efb17ffa89c8ce2
jaredh123 <jared.hanson12345@gmail.com>
2019-04-05 09:30:05 -0700

    Create new component

---
+++ src/app/ang-website-new/ang-website-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ang-website-new',
+  templateUrl: './ang-website-new.component.html',
+  styleUrls: ['./ang-website-new.component.css']
+})
+export class AngWebsiteNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngWebsiteListComponent } from './ang-website-list/ang-website-list.component';
 import { AngWebsiteEditComponent } from './ang-website-edit/ang-website-edit.component';
+import { AngWebsiteNewComponent } from './ang-website-new/ang-website-new.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AngWebsiteListComponent,
-    AngWebsiteEditComponent
+    AngWebsiteEditComponent,
+    AngWebsiteNewComponent
   ],
   imports: [
     BrowserModule
