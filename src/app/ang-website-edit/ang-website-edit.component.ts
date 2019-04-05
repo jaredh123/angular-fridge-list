387fb720873e28eeac3068cfac11c8490ee333bb
jaredh123 <jared.hanson12345@gmail.com>
2019-04-05 08:58:26 -0700

    Create edit component

---
+++ src/app/ang-website-edit/ang-website-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ang-website-edit',
+  templateUrl: './ang-website-edit.component.html',
+  styleUrls: ['./ang-website-edit.component.css']
+})
+export class AngWebsiteEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AngWebsiteListComponent } from './ang-website-list/ang-website-list.component';
+import { AngWebsiteEditComponent } from './ang-website-edit/ang-website-edit.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AngWebsiteListComponent
+    AngWebsiteListComponent,
+    AngWebsiteEditComponent
   ],
   imports: [
     BrowserModule
